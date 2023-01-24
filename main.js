09d8d8f8590d2417ea5cf2bfad33a624878d7732
Ken Chan <chanken20012001@gmail.com>
2023-01-24 14:55:43 +0800

    Updated pop-up window and collapsibles

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const collapsibles = document.querySelectorAll(".collapsible");
+console.log(collapsibles);
 collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
     this.classList.toggle("collapsible--expanded");
@@ -6,27 +7,32 @@ collapsibles.forEach((item) =>
 );
 
 let nav = document.querySelector("nav");
-let blur = document.querySelector(".website");
-let feeback = document.querySelector(".feeback");
+let blur = document.querySelector(".blur");
+
 let new__trigger = document.querySelector(".new__trigger");
 let new__div = document.querySelector(".new__div");
 nav.addEventListener("click", (e) => {
   let target = e.target;
   if (target.classList.contains("suggestion")) {
-    blur.classList.toggle("blur");
-    feeback.classList.toggle("open");
+    console.log("g");
+    blur.classList.toggle("open");
   }
   if (target.classList.contains("new__trigger")) {
     new__div.classList.toggle("open--block");
   }
 });
 
+blur.addEventListener("click", (e) => {
+  if (e.target.classList.contains("blur")) {
+    e.target.classList.toggle("open");
+  }
+});
+
 let close = document.querySelectorAll(".close");
 close.forEach((button) => {
-  button.addEventListener("click", (e) => {
+  button.addEventListener("click", () => {
     console.log(button);
-    e.target.parentNode.parentNode.classList.toggle("open");
-    blur.classList.toggle("blur");
+    blur.classList.toggle("open");
   });
 });
 
